05747ac5665c5b974331824136602f15390a9698
Antonio Serrano <[jose.serrano@yoin.com.mx]>
2019-12-09 20:55:39 -0600

    commit inicial

---
+++ AngularT/02-spa/src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+
+  heroe:any = {};
+
+  constructor( private activatedRouted: ActivatedRoute,
+                private _heroesService: HeroesService
+  
+  ) { 
+    
+    this.activatedRouted.params.subscribe( params =>{
+        this.heroe = this._heroesService.getHeroe( params['id'] );
+    })
+  }
+ 
+  
+
+}
